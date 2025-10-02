24c904f75c58402c826075c909e7fef892d0ad6a
eZurn <ezurn0525@gmail.com>
2025-10-02 13:44:04 -0400

    Random

--- TetrisStarterCode/src/controllers/TetrisController.java
+++ TetrisStarterCode/src/controllers/TetrisController.java
@@ -3,6 +3,7 @@ package controllers;
 import models.StraightLine;
 import models.Tetronimo;
 import views.TetrisBoard;
+import models.TetronimoFactory;
 
 /**
  * TetrisController.java:
@@ -32,9 +33,8 @@ public class TetrisController
      */
     public Tetronimo getNextTetromino()
     {
-        Tetronimo tetronimo;
+        Tetronimo tetronimo = TetronimoFactory.getRandomTetronimo();
 
-        tetronimo = new StraightLine();
         tetronimo.setLocation( 40 + (5 * Tetronimo.SIZE), 0 );
 
         return tetronimo;
