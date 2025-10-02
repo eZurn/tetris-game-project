6d8931a704917fde2f5dc625eae97f67231650fd
matthewnguyen <matt.nguyen814@gmail.com>
2025-10-02 13:06:17 -0400

    OShape

---
+++ TetrisStarterCode/src/models/OShape.java
@@ -0,0 +1,25 @@
+package models;
+import java.awt.Point;
+
+    public class OShape extends Tetronimo {
+
+        public OShape() {
+            super.r1.setLocation(0, 0);
+            super.r2.setLocation(0, Tetronimo.SIZE);
+            super.r3.setLocation(Tetronimo.SIZE, 0);
+            super.r4.setLocation(Tetronimo.SIZE, Tetronimo.SIZE);
+
+            super.add(r1);
+            super.add(r2);
+            super.add(r3);
+            super.add(r4);
+        }
+
+        public int getHeight() {
+            return 2;
+        }
+
+        public int getWidth() {
+            return 2;
+        }
+    }
