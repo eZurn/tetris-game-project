a5520ef66933f77c734870a2cb587c17888904c3
adamh <adamhsmith06@gmail.com>
2025-10-23 15:52:30 -0400

    Lshape2

--- TetrisStarterCode/src/models/TetronimoFactory.java
+++ TetrisStarterCode/src/models/TetronimoFactory.java
@@ -7,9 +7,10 @@ public class TetronimoFactory {
 
     public static Tetronimo getRandomTetronimo() {
         Tetronimo[] shapes = {
-                new OShape(),
-                new StraightLine(),
-                new LShape1()
+                //new OShape(),
+                //new StraightLine(),
+                //new LShape1(),
+                new LShape2()
                 // Add more shapes here when you create them, e.g., new LShape(), new JShape(), etc.
         };
         return shapes[random.nextInt(shapes.length)];
